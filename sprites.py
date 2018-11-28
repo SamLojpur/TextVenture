71898a8e56009cdbfcbbce9a571c28372cb82da4
cmput274 <cmput274@ualberta.ca>
2018-11-27 20:30:03 -0700

    no longer invisible yo

--- sprites.py
+++ sprites.py
@@ -12,4 +12,6 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("images/character.PNG")
-        self.rect = self.image.get_rect()   
+        self.rect = self.image.get_rect()  
+        self.rect.x = 200
+        self.rect.y = 200
